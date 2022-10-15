897161a0325110f2e125b588041c47c00a5844e7
tryiou <73646876+tryiou@users.noreply.github.com>
2022-10-15 12:39:18 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 discord_token = '' #string
 discord_channel = 66666666666666 #int
 exr_endpoint1 = "http://x.x.x.x/xrs/"
-exr_endpoint2 = "http://x.x.x.x/xrs/"  # utils.blocknet.org/xrs/"
+exr_endpoint2 = "https://utils.blocknet.org/xrs/"
 cc_endpoint1= "https://utils.blocknet.org/xrs/"
 xr_endpoint1= "http://utils.blocknet.org/xr/"
